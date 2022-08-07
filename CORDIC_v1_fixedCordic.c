68360f8008a75078074c84efdfe6c0a5b961cc2e
Jiazhuo Jiang <jiazhuojiang@uvic.ca>
2022-08-07 00:38:20 -0700

    version 1 add comment

--- CORDIC_v1_fixedCordic.c
+++ CORDIC_v1_fixedCordic.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int list_size = 16;
-int SHIFTBITS = 12;
-int CONSTANT = 2487;
-double FIXEDBASE = 4096.0;
+//global variable
+int list_size = 16; // Size of the check list
+int SHIFTBITS = 12; // 12bits magnitude
+int CONSTANT = 2487; // constant for calculate the output (0.6072529351031395 Fixed)
+double FIXEDBASE = 4096.0; // 2^12
 
-int arctan_engle_2[] = {
-    184320, //
-    108810,
-    57492,
-    29184,
-    14648,
-    7331,
-    3666,
-    1833,
-    916,
-    458,
-    229,
-    114,
-    57,
-    28,
-    14,
-    7,
+// Check List
+int arctan_angle[] = {
+    184320, // 45.0
+    108810, // 26.56505117707799
+    57492,  // 14.036243467926479
+    29184,  // 7.125016348901798
+    14648,  // 3.576334374997351
+    7331,   // 1.7899106082460694
+    3666,   // 0.8951737102110744
+    1833,   // 0.4476141708605531
+    916,    // 0.22381050036853808
+    458,    // 0.1119056770662069
+    229,    // 0.055952891893803675
+    114,    // 0.027976452617003676
+    57,     // 0.013988227142265016
+    28,     // 0.006994113675352919
+    14,     // 0.003497056850704011
+    7,      // 0.0017485284269804495
 };
 
+// Define a data type to indicate the mode of the CORDIC function
 typedef enum MODE
 {
     COS,
@@ -32,26 +35,38 @@ typedef enum MODE
     ARCTAN
 } MODE;
 
+
+// covert float to fixed
 int float_to_fixed(double flt)
 {
     return (int)(flt * FIXEDBASE);
 }
 
+// convert fixed to float
 double fixed_to_float(int fixed)
 {
     return fixed / FIXEDBASE;
 }
 
+// based on the mode indicator, and the value of y or theta to make the decision tree
 int ModeDecision(MODE mode, int y, int angle)
 {
     if (mode == ARCTAN && y < 0)
         return 1;
     else if (mode != ARCTAN && angle > 0)
         return 1;
-    else
+    else // Rotation mode and y>0 or Vector mode and angle<0
         return 0;
 }
 
+/*
+    CORDIC
+    Input double x,y, theta, and mode
+    Step 1: convert the double x,y to 12 bits Fixed-point number
+    Step 2: Calculate the output
+    Step 3: Choose the output value based on the mode
+    Step 4: Convert the fixed-point output into float(double)
+*/
 double cordic(double x, double y, double angle, MODE mode)
 {
     int temp_a, temp_b, a, b, result;
@@ -66,23 +81,23 @@ double cordic(double x, double y, double angle, MODE mode)
         {
             temp_a = a - (b >> i);
             temp_b = b + (a >> i);
-            theta = theta - arctan_engle_2[i];
+            theta = theta - arctan_angle[i];
         }
         else
         {
             temp_a = a + (b >> i);
             temp_b = b - (a >> i);
-            theta = theta + arctan_engle_2[i];
+            theta = theta + arctan_angle[i];
         }
         a = temp_a;
         b = temp_b;
     }
 
     if (mode == SIN)
-        result = (b * CONSTANT) >> 12;
+        result = (b * CONSTANT) >> SHIFTBITS;
 
     else if (mode == COS)
-        result = (a * CONSTANT) >> 12;
+        result = (a * CONSTANT) >> SHIFTBITS;
 
     else
         result = theta;
@@ -92,6 +107,7 @@ double cordic(double x, double y, double angle, MODE mode)
 
 int main(int argc, char const *argv[])
 {
+    // test values
     double test_sin = cordic(1, 0, 70, SIN);
     printf("sin 70 = %f\n", test_sin);
 
